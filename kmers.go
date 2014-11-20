3dce7437b3cf9ac2ea050831dc4750614a96b718
vtphan <vphan@memphis.edu>
2014-11-19 19:42:10 -0600

    simplify kmers generation

--- kmers.go
+++ kmers.go
@@ -44,15 +44,16 @@ h := map[byte]int{'A':0, 'C':1, 'G':2, 'T':3}
 id = (id<<2) - (h[sequence[i-1]] << (uint(2*K))) + h[sequence[i+K-1]]
 */
 func Kmers(sequence []byte, K int, start int, end int, result chan int) {
-   i := firstIndex(sequence, K, start, end)
-   if i == -1 {
-      return
-   }
    uK := uint(K)
-   id := Kmer2Num(sequence, K, i)
-   result <- id
-   for i:=i+1; i<end-K+1; i++ {
-      if sequence[i+K-1]=='A' || sequence[i+K-1]=='C' || sequence[i+K-1]=='G' || sequence[i+K-1]=='T' {
+   var id int
+   for i:=start; i<end-K+1; i++ {
+      if i==start || sequence[i+K-1]=='N' {
+         i = firstIndex(sequence, K, i, end)
+         if i==-1 {
+            return
+         }
+         id = Kmer2Num(sequence, K, i)
+      } else {
          switch sequence[i-1] {
             case 'A': id = (id<<2)
             case 'C': id = (id<<2) - (1 << (uK<<1))
@@ -65,15 +66,8 @@ func Kmers(sequence []byte, K int, start int, end int, result chan int) {
             case 'G': id += 2
             case 'T': id += 3
          }
-         result <- id
-      } else {
-         i = firstIndex(sequence, K, i, end)
-         if i==-1 {
-            return
-         }
-         id = Kmer2Num(sequence, K, i)
-         result <- id
       }
+      result <- id
    }
 }
 
