91189617e47d592058368f79b50119b237b53c63
vtphan <vphan@memphis.edu>
2015-02-05 11:40:08 -0600

    .

--- examples/slider.go
+++ examples/slider.go
@@ -30,7 +30,7 @@ func FreqFromReads(readFile string) {
    // Spread the processing of reads into different cores
    result := make(chan int)
    numCores := 4
-   K := 5
+   K := 4
    var wg sync.WaitGroup
    for i:=0; i<numCores; i++ {
       wg.Add(1)
