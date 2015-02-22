6ca29bf74c8d2766f54b910de84ea44344ffdfc2
vtphan <vphan@memphis.edu>
2015-02-22 17:06:29 -0600

    .

--- examples/counting_some_kmers.go
+++ examples/counting_some_kmers.go
@@ -29,18 +29,21 @@ func CountFreq(readFile string, K int) {
       close(reads)
    }()
 
-   // Spread the processing of reads into different cores
    numCores := runtime.NumCPU()
    runtime.GOMAXPROCS(numCores)
    var wg sync.WaitGroup
 
+   // Here is a map of kmers that need to be counted.  Only these kmers are counted.
    freq := make(map[int]int)
    freq[158] = 0
    freq[180] = 0
    freq[39] = 0
    freq[59] = 0
+
+   // Start a new counter that counts only kmers in freq.
    c := kmers.NewKmerCounter(K, freq)
 
+   // Count kmers in different cores simultaneously.
    for i:=0; i<numCores; i++ {
       wg.Add(1)
       go func() {
@@ -51,8 +54,10 @@ func CountFreq(readFile string, K int) {
       }()
    }
 
+   // Finish counting
    wg.Wait()
 
+   // Print out the result
    for kmer := range(freq) {
       fmt.Println(kmer,kmers.NumToKmer(kmer,K),freq[kmer])
    }
